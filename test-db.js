2e23e644b2bd29dc233745c268b3e7cf46030d2b
sadi21863-bit <sadi21863@gmail.com>
2026-04-22 18:31:42 +0530

    chore: remove test-db.js diagnostic script

--- test-db.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const postgres = require('postgres');
-const sql = postgres(process.env.DATABASE_URL);
-sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY 1`
-    .then(r => { r.forEach(x => console.log(x.table_name)); process.exit(); })
-    .catch(e => { console.error(e.message); process.exit(1); });
